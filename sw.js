74d10ddc10357739fcacf24fad84a6ff2f0866e0
Malwin-S <malwinsawant@gmail.com>
2026-03-12 15:50:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'existential-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
